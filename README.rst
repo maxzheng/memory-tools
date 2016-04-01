fb5a8f1584657dd801f3a68962963331617150c9
Max Zheng <mzheng@linkedin.com>
2016-04-01 15:17:19 -0700

    Initial setup

---
+++ README.rst
@@ -0,0 +1,21 @@
+memory-tools
+========================
+
+A set of simple yet effective tools to troubleshoot memory leaks.
+
+Quick Start Tutorial
+====================
+
+TBD
+
+Links & Contact Info
+====================
+
+| Documentation: http://memory-tools.readthedocs.org
+|
+| PyPI Package: https://pypi.python.org/pypi/memory-tools
+| GitHub Source: https://github.com/maxzheng/memory-tools
+| Report Issues/Bugs: https://github.com/maxzheng/memory-tools/issues
+|
+| Connect: https://www.linkedin.com/in/maxzheng
+| Contact: maxzheng.os @t gmail.com
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import setuptools
+
+
+setuptools.setup(
+  name='memory-tools',
+  version='0.0.1',
+
+  author='Max Zheng',
+  author_email='maxzheng.os @t gmail.com',
+
+  description='A set of simple yet effective tools to troubleshoot memory leaks.',
+  long_description=open('README.rst').read(),
+
+  url='https://github.com/maxzheng/memory-tools',
+
+#  entry_points={
+#    'console_scripts': [
+#      'script_name = package.module:entry_callable',
+#    ],
+#  },
+
+  install_requires=open('requirements.txt').read(),
+
+  license='MIT',
+
+  package_dir={'': 'src'},
+  packages=setuptools.find_packages('src'),
+  include_package_data=True,
+
+  setup_requires=['setuptools-git'],
+
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+
+  keywords='memory tools leaks profiler simple easy to use process usage',
+)
